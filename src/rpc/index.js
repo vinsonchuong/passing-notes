35398a469e418ec1fe981544bc9621c8a354c99b
Vinson Chuong <vinsonchuong@gmail.com>
2018-06-08 07:20:33 -0700

    refactor(serveRpc): Expose serveRpc from lib/middleware

--- src/rpc/index.js
+++ src/rpc/index.js
@@ -1,3 +1,3 @@
 /* @flow */
 export { default as sendRpc } from './send-rpc'
-export { liftResponder as serveRpc } from 'passing-notes/lib/rpc'
+export { serveRpc } from 'passing-notes/lib/middleware'
