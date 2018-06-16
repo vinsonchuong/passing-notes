828a4b20e4b0a7ba3aef324dff45dd52d59505e7
Vinson Chuong <vinsonchuong@gmail.com>
2018-06-16 12:24:02 -0700

    refactor(serveStatic): Move into lib/serve-static-files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 /* @flow */
 export { sendRpc, serveRpc } from 'passing-notes/lib/rpc'
 export { sendRequest, respondToRequests } from 'passing-notes/lib/http'
-export { serveUi } from 'passing-notes/lib/middleware'
+export { serveUi } from 'passing-notes/lib/bundler'
