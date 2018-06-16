733a57eff056ab627d3c992f27e7386d7e80eb27
Vinson Chuong <vinsonchuong@gmail.com>
2018-06-15 17:44:48 -0700

    refactor(respondToRequests): Move into lib/http

--- src/http/index.js
+++
@@ -1,2 +0,0 @@
-/* @flow */
-export { default as respondToRequests } from './respond-to-requests'
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /* @flow */
-export * from 'passing-notes/src/http'
 export * from 'passing-notes/src/rpc'
-export { sendRequest } from 'passing-notes/lib/http'
+export { sendRequest, respondToRequests } from 'passing-notes/lib/http'
 export { serveUi } from 'passing-notes/lib/middleware'
