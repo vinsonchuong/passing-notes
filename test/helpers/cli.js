ea37b97733b52dec1125a7d7c2ed86aacc14523d
Vinson Chuong <vinsonchuong@gmail.com>
2018-06-17 16:00:40 -0700

    feat(log): Create first pass at logger

---
+++ test/helpers/cli.js
@@ -0,0 +1,51 @@
+/* @flow */
+import type { ChildProcess } from 'child_process'
+import * as childProcess from 'child_process'
+
+type SpawnedProcess = {
+  process: ChildProcess,
+  stdout: string,
+  stderr: string
+}
+
+export async function start(
+  command: Array<string>,
+  options: { cwd?: string, env?: { [string]: string }, waitForOutput: string }
+): Promise<SpawnedProcess> {
+  const spawned = {
+    process: childProcess.spawn(command[0], command.slice(1), {
+      ...options,
+      env: { ...process.env, ...options.env },
+      detached: true
+    }),
+    stdout: '',
+    stderr: ''
+  }
+
+  await new Promise((resolve, reject) => {
+    spawned.process.stdout.setEncoding('utf8')
+    spawned.process.stderr.setEncoding('utf8')
+
+    spawned.process.stderr.on('data', chunk => {
+      spawned.stderr += chunk
+    })
+    spawned.process.stdout.on('data', chunk => {
+      spawned.stdout += chunk
+      if (chunk.includes(options.waitForOutput)) {
+        resolve()
+      }
+    })
+    spawned.process.once('close', code => {
+      reject(new Error(`Command exited with code ${code}`))
+    })
+  })
+
+  return spawned
+}
+
+export function stop(spawned: SpawnedProcess): Promise<void> {
+  process.kill(-spawned.process.pid)
+  return new Promise(resolve => {
+    spawned.process.once('close', () => resolve())
+  })
+}
--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,4 +1,5 @@
 /* @flow */
 export { default as defineFixture } from './define-fixture'
 export { default as evaluateInBrowser } from './evaluate-in-browser'
-export { writeFile, listDirectory, start, stop } from './io'
+export * from './cli'
+export * from './fs'
