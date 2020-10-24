4c3875e82dfb6f6274f41c49dc6131c7f61e8781
Vinson Chuong <vinsonchuong@gmail.com>
2020-10-24 11:59:54 -0700

    fix(pass-notes): Disable hot-loading in production

--- bin.js
+++ bin.js
@@ -5,6 +5,10 @@ import run from 'puff-pastry'
 const require = createRequire(import.meta.url)
 const hotEsmPath = require.resolve('hot-esm')
 
-run('./cli/index.js', {
-  flags: ['--loader', hotEsmPath, '--no-warnings']
-})
+if (process.env.NODE_ENV === 'production') {
+  run('./cli/index.js')
+} else {
+  run('./cli/index.js', {
+    flags: ['--loader', hotEsmPath, '--no-warnings']
+  })
+}
--- bin.test.js
+++ bin.test.js
@@ -81,3 +81,74 @@ test('starting a development server with hot-reloading', async (t) => {
     }
   )
 })
+
+test('not enabling hot-reloading in production', async (t) => {
+  const directory = await useTemporaryDirectory(t)
+
+  await install(process.cwd(), directory.path)
+
+  await directory.writeFile(
+    'server.mjs',
+    `
+    export default () => {
+      return {
+        status: 200,
+        headers: {
+          'Content-Type': 'text/plain'
+        },
+        body: 'Hello World!'
+      }
+    }
+    `
+  )
+
+  const server = runProcess(t, {
+    command: ['npx', 'pass-notes', 'server.mjs'],
+    cwd: directory.path,
+    env: {PORT: '10011', NODE_ENV: 'production'}
+  })
+  try {
+    await server.waitForOutput('Listening', 5000)
+  } catch (error) {
+    console.log(server.output)
+    throw error
+  }
+
+  t.like(
+    await sendRequest({
+      method: 'GET',
+      url: 'http://localhost:10011',
+      headers: {}
+    }),
+    {
+      body: 'Hello World!'
+    }
+  )
+
+  await directory.writeFile(
+    'server.mjs',
+    `
+    export default () => {
+      return {
+        status: 200,
+        headers: {
+          'Content-Type': 'text/plain'
+        },
+        body: 'Hello There!'
+      }
+    }
+    `
+  )
+  await wait(500)
+
+  t.like(
+    await sendRequest({
+      method: 'GET',
+      url: 'http://localhost:10011',
+      headers: {}
+    }),
+    {
+      body: 'Hello World!'
+    }
+  )
+})
